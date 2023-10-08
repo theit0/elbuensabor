294745598f99244c18cb68367c89e13fac196d20
Theo Pelegrina <71673834+theit0@users.noreply.github.com>
2023-10-08 19:40:13 -0300

    harcode cambios

--- src/main/java/elbuensabor/entities/RubroArticulo.java
+++ src/main/java/elbuensabor/entities/RubroArticulo.java
@@ -15,21 +15,7 @@ import java.util.List;
 @Setter
 @Builder
 public class RubroArticulo extends Base{
-<<<<<<< HEAD
-    //@ManyToOne(cascade = CascadeType.REFRESH,fetch = FetchType.LAZY)
-    //@JoinColumn(name = "id_rubro_padre")
-    //private RubroArticulo rubroPadre;
 
-    // @OneToMany(mappedBy = "rubroPadre")
-    // private List<RubroArticulo> subRubros;
-=======
-   // @ManyToOne(cascade = CascadeType.REFRESH,fetch = FetchType.LAZY)
-    //@JoinColumn(name = "id_rubro_padre")
-    //private RubroArticulo rubroPadre;
-
-    //@OneToMany(mappedBy = "rubroPadre")
-    //private List<RubroArticulo> subRubros;
->>>>>>> 3c1e28eda01327a048741dd08fd45bdef932e7f7
 
     @NotNull
     private String denominacion;
@@ -43,25 +29,4 @@ public class RubroArticulo extends Base{
     private Date fechaModificacion;
 
 
-
-<<<<<<< HEAD
-    //public RubroArticulo(String denominacion, RubroArticulo rubroPadre) {
-    //    this.denominacion = denominacion;
-    //    this.rubroPadre = rubroPadre;
-    //}
-
-    //public void setSubRubros(List<RubroArticulo> subRubros) {
-    //    this.subRubros.clear();
-    //    this.subRubros.addAll(subRubros);
-=======
-   // public RubroArticulo(String denominacion, RubroArticulo rubroPadre) {
-     //   this.denominacion = denominacion;
-       // this.rubroPadre = rubroPadre;
-   // }
-
-    //public void setSubRubros(List<RubroArticulo> subRubros) {
-      //  this.subRubros.clear();
-       // this.subRubros.addAll(subRubros);
->>>>>>> 3c1e28eda01327a048741dd08fd45bdef932e7f7
-    //}
 }

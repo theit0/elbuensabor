4cf1f5d7c5d5618063f854cbeff6ee651338e570
giovannicirrincione <61981341+giovannicirrincione@users.noreply.github.com>
2023-10-25 15:40:16 -0300

    Update FacturaController.java

--- src/main/java/elbuensabor/controllers/FacturaController.java
+++ src/main/java/elbuensabor/controllers/FacturaController.java
@@ -10,4 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/factura")
 public class FacturaController extends BaseControllerImpl<Factura, FacturaServiceImpl>{
+  @PostMapping("/Facturar")
+    public ResponseEntity FacturarPedido(@RequestParam Long id){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body("Factura Creada");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"error\":\"" + e.getMessage() + "\"}"));
+
+        }
+    }
 }

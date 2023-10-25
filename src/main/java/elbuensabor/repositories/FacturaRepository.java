375c2211e5f13ce05a5d06fec0040e17809b8400
giovannicirrincione <61981341+giovannicirrincione@users.noreply.github.com>
2023-10-25 15:40:58 -0300

    Update FacturaRepository.java

--- src/main/java/elbuensabor/repositories/FacturaRepository.java
+++ src/main/java/elbuensabor/repositories/FacturaRepository.java
@@ -5,4 +5,23 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FacturaRepository extends BaseRepository<Factura,Long>{
+  @Query(
+            value = "INSERT INTO Factura (ID_PEDIDO, FECHA_FACTURACION, TOTAL_VENTA) "+
+                    "SELECT "+
+                    "ID AS ID_PEDIDO, "+
+                    "CURRENT_DATE AS FECHA_FACTURACION, "+
+                    "Total AS TOTAL_VENTA "+
+                    "FROM pedido "+
+                    "WHERE ID =:id AND Estado = 'PAGADO'; "+
+
+                    "INSERT INTO Detalle_Factura (ID_Factura, ID_ARTICULO_MANUFACTURADO, Cantidad, SUBTOTAL) "+
+                    "SELECT ID FROM FACTURA AS ID_FACTURA, "+
+                    "ID_ARTICULO_MANUFACTURADO, "+
+                    "CANTIDAD, "+
+                    "SUBTOTAL " +
+                    "FROM Detalle_Pedido "+
+                    "WHERE ID_Pedido =:id ",
+            nativeQuery = true
+    )
+    Factura FacturarPedido(@Param("id")Long id);
 }

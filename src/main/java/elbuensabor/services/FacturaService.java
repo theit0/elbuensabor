fcbcc0c49079ae1143fefbac8c2ec2a3d7ea5421
giovannicirrincione <61981341+giovannicirrincione@users.noreply.github.com>
2023-10-25 15:41:51 -0300

    Update FacturaService.java

--- src/main/java/elbuensabor/services/FacturaService.java
+++ src/main/java/elbuensabor/services/FacturaService.java
@@ -3,4 +3,5 @@ package elbuensabor.services;
 import elbuensabor.entities.Factura;
 
 public interface FacturaService extends BaseService<Factura,Long>{
+  void FacturarPedido(Long id) throws Exception;
 }

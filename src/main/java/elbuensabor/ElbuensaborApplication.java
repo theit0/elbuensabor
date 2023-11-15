6501c91a55aadfe1852d4aa8b4f6ed437ca4102c
giovannicirrincione <giovannicirrincione22@gmail.com>
2023-11-15 11:58:13 -0300

    ultimo

--- src/main/java/elbuensabor/ElbuensaborApplication.java
+++ src/main/java/elbuensabor/ElbuensaborApplication.java
@@ -54,49 +54,167 @@ public class ElbuensaborApplication {
 		return args -> {
 			UnidadMedida mililitros = UnidadMedida.builder()
 					.abreviatura("ml")
-					.denominacion("Milimetros")
+					.denominacion("Mililitros")
 					.fechaAlta(LocalDateTime.now())
 					.build();
 			unidadMedidaRepository.save(mililitros);
-			RubroArticulo ComidaRapida = RubroArticulo.builder()
+
+			UnidadMedida gramos = UnidadMedida.builder()
+					.abreviatura("gr")
+					.denominacion("Gramos")
+					.fechaAlta(LocalDateTime.now())
+					.build();
+			unidadMedidaRepository.save(gramos);
+
+			UnidadMedida unidades = UnidadMedida.builder()
+					.abreviatura("unidad")
+					.denominacion("Unidades")
+					.fechaAlta(LocalDateTime.now())
+					.build();
+			unidadMedidaRepository.save(unidades);
+
+			UnidadMedida litros = UnidadMedida.builder()
+					.abreviatura("l")
+					.denominacion("Litros")
+					.fechaAlta(LocalDateTime.now())
+					.build();
+			unidadMedidaRepository.save(litros);
+
+			UnidadMedida kilogramos = UnidadMedida.builder()
+					.abreviatura("kg")
+					.denominacion("Kilogramos")
+					.fechaAlta(LocalDateTime.now())
+					.build();
+			unidadMedidaRepository.save(kilogramos);
+			UnidadMedida unidadDocena = UnidadMedida.builder()
+					.abreviatura("docena")
+					.denominacion("Docena")
+					.fechaAlta(LocalDateTime.now())
+					.build();
+			unidadMedidaRepository.save(unidadDocena);
+
+			RubroArticulo comidaRapida = RubroArticulo.builder()
 					.denominacion("Comida Rapida")
 					.fechaAlta(LocalDateTime.now())
 					.build();
-			rubroArticuloRepository.save(ComidaRapida);
-			RubroArticulo ComidaSana = RubroArticulo.builder()
-					.denominacion("Comida Sana")
+			rubroArticuloRepository.save(comidaRapida);
+
+			RubroArticulo Vegetales = RubroArticulo.builder()
+					.denominacion("Vegetales")
 					.fechaAlta(LocalDateTime.now())
 					.build();
-			rubroArticuloRepository.save(ComidaSana);
+			rubroArticuloRepository.save(Vegetales);
+
 			RubroArticulo bebidas = RubroArticulo.builder()
-					.denominacion("bebidas")
+					.denominacion("Bebidas")
 					.fechaAlta(LocalDateTime.now())
 					.build();
 			rubroArticuloRepository.save(bebidas);
+
+			ArticuloInsumo articulo1 = ArticuloInsumo.builder()
+					.denominacion("Pan")
+					.fechaAlta(LocalDateTime.now())
+					.precioCompra(BigDecimal.valueOf(60))
+					.stockActual(BigDecimal.valueOf(100))
+					.stockMinimo(BigDecimal.valueOf(50))
+					.urlImagen("https://t2.uc.ltmcdn.com/es/posts/8/3/6/como_hacer_pan_de_hamburguesa_32638_600.jpg")
+					.rubroArticulo(comidaRapida)
+					.unidadMedida(unidades)
+					.build();
+			articuloInsumoRepository.save(articulo1);
+
+			ArticuloInsumo articulo2 = ArticuloInsumo.builder()
+					.denominacion("Queso cheddar")
+					.fechaAlta(LocalDateTime.now())
+					.precioCompra(BigDecimal.valueOf(20))
+					.stockActual(BigDecimal.valueOf(80))
+					.stockMinimo(BigDecimal.valueOf(40))
+					.urlImagen("https://http2.mlstatic.com/D_NQ_NP_678367-MLA45862458371_052021-O.webp")
+					.rubroArticulo(comidaRapida)
+					.unidadMedida(unidades)
+					.build();
+			articuloInsumoRepository.save(articulo2);
+
+			ArticuloInsumo articulo3 = ArticuloInsumo.builder()
+					.denominacion("Lechuga")
+					.fechaAlta(LocalDateTime.now())
+					.precioCompra(BigDecimal.valueOf(40))
+					.stockActual(BigDecimal.valueOf(20))
+					.stockMinimo(BigDecimal.valueOf(2))
+					.urlImagen("https://biotrendies.com/wp-content/uploads/2015/07/lechuga-1200x900.jpg")
+					.rubroArticulo(Vegetales)
+					.unidadMedida(kilogramos)
+					.build();
+			articuloInsumoRepository.save(articulo3);
+
+
+
+
+			// Agregar más rubros
+			RubroArticulo postres = RubroArticulo.builder()
+					.denominacion("Postres")
+					.fechaAlta(LocalDateTime.now())
+					.build();
+			rubroArticuloRepository.save(postres);
+
+			RubroArticulo bebidasAlcoholicas = RubroArticulo.builder()
+					.denominacion("Bebidas Alcohólicas")
+					.fechaAlta(LocalDateTime.now())
+					.build();
+			rubroArticuloRepository.save(bebidasAlcoholicas);
+
+			ArticuloInsumo harina = ArticuloInsumo.builder()
+					.denominacion("Harina")
+					.fechaAlta(LocalDateTime.now())
+					.precioCompra(BigDecimal.valueOf(30))
+					.stockActual(BigDecimal.valueOf(200))
+					.stockMinimo(BigDecimal.valueOf(100))
+					.urlImagen("https://iili.io/JCvwiNV.png")
+					.rubroArticulo(comidaRapida)
+					.unidadMedida(kilogramos)
+					.build();
+			articuloInsumoRepository.save(harina);
+
+			ArticuloInsumo huevo = ArticuloInsumo.builder()
+					.denominacion("Huevo")
+					.fechaAlta(LocalDateTime.now())
+					.precioCompra(BigDecimal.valueOf(5))
+					.stockActual(BigDecimal.valueOf(300))
+					.stockMinimo(BigDecimal.valueOf(150))
+					.urlImagen("https://iili.io/JCvwPAQ.png")
+					.rubroArticulo(postres)
+					.unidadMedida(unidadDocena)
+					.build();
+			articuloInsumoRepository.save(huevo);
+
+
+
+
+
+
+
+
+
+
+
+
 			RubroArticulo bebidasinalcohol1 = RubroArticulo.builder()
-					.denominacion("bebidas sin alcohol")
+					.denominacion("Bebidas sin alcohol")
 					.fechaAlta(LocalDateTime.now())
 					.rubroPadre(bebidas)
 					.build();
 			rubroArticuloRepository.save(bebidasinalcohol1);
 			RubroArticulo bebidaconalcohol1 = RubroArticulo.builder()
-					.denominacion("bebidas con alcohol")
+					.denominacion("Bebidas con alcohol")
 					.fechaAlta(LocalDateTime.now())
 					.rubroPadre(bebidas)
 					.build();
 			rubroArticuloRepository.save(bebidaconalcohol1);
 
-			ArticuloManufacturado Cocacola = ArticuloManufacturado.builder()
-					.denominacion("Cocacola 500ml")
-					.costo(new BigDecimal(500))
-					.precioVenta(new BigDecimal(800))
-					.urlImagen("https://i.ibb.co/nDjVtXX/pngwing-com-2-1.png")
-					.fechaAlta(LocalDateTime.now())
-					.rubroArticulo(bebidasinalcohol1)
-					.build();
-			articuloManufacturadoRepository.save(Cocacola);
+
 			ArticuloManufacturado Fanta = ArticuloManufacturado.builder()
 					.denominacion("Fanta 500ml")
+					.descripcion("Bebida gasificada")
 					.costo(new BigDecimal(500))
 					.precioVenta(new BigDecimal(800))
 					.urlImagen("https://i.ibb.co/2h4WF2r/pngwing-com-3-1.png")
@@ -106,6 +224,7 @@ public class ElbuensaborApplication {
 			articuloManufacturadoRepository.save(Fanta);
 			ArticuloManufacturado Sprite = ArticuloManufacturado.builder()
 					.denominacion("Sprite 500ml")
+					.descripcion("Bebida gasificada")
 					.costo(new BigDecimal(500))
 					.precioVenta(new BigDecimal(800))
 					.urlImagen("https://i.ibb.co/z75JBp3/pngwing-com-4-1.png")
@@ -116,6 +235,7 @@ public class ElbuensaborApplication {
 			articuloManufacturadoRepository.save(Sprite);
 			ArticuloManufacturado Cerveza = ArticuloManufacturado.builder()
 					.denominacion("Cerveza Heineken")
+					.descripcion("Bebida alcholica gasificada")
 					.costo(new BigDecimal(700))
 					.precioVenta(new BigDecimal(1000))
 					.urlImagen("https://i.ibb.co/0Jwv8dT/pngwing-com-11-1.png")
@@ -131,7 +251,7 @@ public class ElbuensaborApplication {
 					.precioVenta(new BigDecimal(2000))
 					.tiempoEstimadoCocina(50)
 					.urlImagen("https://i.ibb.co/Srv0rhf/pngwing-com-5-1.png")
-					.rubroArticulo(ComidaRapida)
+					.rubroArticulo(comidaRapida)
 					.build();
 			articuloManufacturadoRepository.save(Hamburguesa);
 			ArticuloManufacturado Taco = ArticuloManufacturado.builder()
@@ -142,7 +262,7 @@ public class ElbuensaborApplication {
 					.precioVenta(new BigDecimal(1000))
 					.tiempoEstimadoCocina(30)
 					.urlImagen("https://i.ibb.co/grXmj7F/pngwing-com-7-1.png")
-					.rubroArticulo(ComidaSana)
+					.rubroArticulo(comidaRapida)
 					.build();
 
 			articuloManufacturadoRepository.save(Taco);
@@ -154,53 +274,34 @@ public class ElbuensaborApplication {
 					.precioVenta(new BigDecimal(2000))
 					.tiempoEstimadoCocina(40)
 					.urlImagen("https://i.ibb.co/gSjM1PY/pngwing-com-6-1.png")
-					.rubroArticulo(ComidaSana)
+					.rubroArticulo(comidaRapida)
 					.build();
 			articuloManufacturadoRepository.save(Lomo);
 			ArticuloManufacturado PizzaMuzza = ArticuloManufacturado.builder()
 					.costo(new BigDecimal(750))
 					.denominacion("Pizza muzzarela")
-					.descripcion("8 poriones. Ideal para 2 personas")
+					.descripcion("8 porciones. Ideal para 2 personas")
 					.fechaAlta(LocalDateTime.now())
 					.precioVenta(new BigDecimal(1500))
 					.tiempoEstimadoCocina(10)
 					.urlImagen("https://i.ibb.co/rc88348/pngwing-com-8-1.png")
-					.rubroArticulo(ComidaRapida)
+					.rubroArticulo(comidaRapida)
 					.build();
 			articuloManufacturadoRepository.save(PizzaMuzza);
 			ArticuloManufacturado PizzaEspe = ArticuloManufacturado.builder()
 					.costo(new BigDecimal(750))
 					.denominacion("Pizza Peperoni")
-					.descripcion("8 poriones. Ideal para 2 personas")
+					.descripcion("8 porciones. Ideal para 2 personas")
 					.fechaAlta(LocalDateTime.now())
 					.precioVenta(new BigDecimal(1500))
 					.tiempoEstimadoCocina(10)
 					.urlImagen("https://i.ibb.co/ZG3YR0k/pngwing-com-9-1.png")
-					.rubroArticulo(ComidaRapida)
+					.rubroArticulo(comidaRapida)
 					.build();
 			articuloManufacturadoRepository.save(PizzaEspe);
-			ArticuloManufacturado EmpanadasCarne = ArticuloManufacturado.builder()
-					.costo(new BigDecimal(800))
-					.denominacion("6 empanadas de carne")
-					.descripcion("6 empanadas. Ideal para 1 persona")
-					.fechaAlta(LocalDateTime.now())
-					.precioVenta(new BigDecimal(1600))
-					.tiempoEstimadoCocina(20)
-					.urlImagen("https://i.ibb.co/ZgL0xZg/pngwing-com-10-1.png")
-					.rubroArticulo(ComidaRapida)
-					.build();
-			articuloManufacturadoRepository.save(EmpanadasCarne);
-			ArticuloManufacturado EmpanadasJamon = ArticuloManufacturado.builder()
-					.costo(new BigDecimal(800))
-					.denominacion("6 empanadas de Jamon y queso")
-					.descripcion("6 empanadas. Ideal para 1 persona")
-					.fechaAlta(LocalDateTime.now())
-					.precioVenta(new BigDecimal(1600))
-					.tiempoEstimadoCocina(15)
-					.urlImagen("https://i.ibb.co/ZgL0xZg/pngwing-com-10-1.png")
-					.rubroArticulo(ComidaRapida)
-					.build();
-			articuloManufacturadoRepository.save(EmpanadasJamon);
+
+
+
 
 
 
@@ -219,47 +320,8 @@ public class ElbuensaborApplication {
 					.usuario(usuario1)
 					.build();
 			clienteRepository.save(cliente1);
-			Domicilio domicilio1 = Domicilio.builder()
-					.calle("la pampa")
-					.codPostal(5507)
-					.fechaAlta(LocalDateTime.now())
-					.localidad("Lujan")
-					.numero(313)
-					.cliente(cliente1)
-					.build();
-			domicilioRepository.save(domicilio1);
-			LocalTime horaEspecifica = LocalTime.of(22, 30, 0);
-			Pedido pedido1 = Pedido.builder()
-					.cliente(cliente1)
-					.domicilioEntrega(domicilio1)
-					.estado(EstadoPedido.PENDIENTE_ENTREGA)
-					.tipoEnvio(TipoEnvio.DELIVERY)
-					.totalCosto(new BigDecimal(8000))
-					.total(new BigDecimal(16000))
-					.fechaPedido(LocalDate.of(2023, 10, 1))
-					.fechaAlta(LocalDateTime.now())
-					.horaEstimadaFinalizacion(horaEspecifica)
-					.build();
-			pedidoRepository.save(pedido1);
-			DetallePedido detalle1 = DetallePedido.builder()
-					.cantidad(4)
-					.articuloManufacturado(Cocacola)
-					.pedido(pedido1)
-					.build();
-			detallePedidoRepository.save(detalle1);
-			Factura factura1 = Factura.builder()
-					.fechaAlta(LocalDateTime.now())
-					.fechaFacturacion(LocalDateTime.now())
-					.formaPago(FormaPago.EFECTIVO)
-					.pedido(pedido1)
-					.build();
-			facturaRepository.save(factura1);
-			DetalleFactura detallefac1 = DetalleFactura.builder()
-					.cantidad(4)
-					.articuloManufacturado(Cocacola)
-					.factura(factura1)
-					.build();
-			detalleFacturaRepository.save(detallefac1);
+
+
 			Usuario usuario2 = Usuario.builder()
 					.username("theoP")
 					.fechaAlta(LocalDateTime.now())
@@ -274,101 +336,6 @@ public class ElbuensaborApplication {
 					.usuario(usuario2)
 					.build();
 			clienteRepository.save(cliente2);
-			Domicilio domicilio2 = Domicilio.builder()
-					.calle("la plata")
-					.codPostal(5507)
-					.fechaAlta(LocalDateTime.now())
-					.localidad("vistalba")
-					.numero(444)
-					.cliente(cliente2)
-					.build();
-			domicilioRepository.save(domicilio2);
-			LocalTime horaEspecifica1 = LocalTime.of(22, 30, 0);
-			Pedido pedido2 = Pedido.builder()
-					.cliente(cliente2)
-					.domicilioEntrega(domicilio1)
-					.estado(EstadoPedido.PAGADO)
-					.tipoEnvio(TipoEnvio.DELIVERY)
-					.fechaAlta(LocalDateTime.now())
-					.totalCosto(new BigDecimal(1000))
-					.total(new BigDecimal(2000))
-					.fechaPedido(LocalDate.of(2023, 10, 10))
-					.horaEstimadaFinalizacion(horaEspecifica)
-					.build();
-			pedidoRepository.save(pedido2);
-			DetallePedido detalle2 = DetallePedido.builder()
-					.cantidad(4)
-					.articuloManufacturado(Cocacola)
-					.pedido(pedido1)
-					.build();
-			detallePedidoRepository.save(detalle2);
-			Pedido pedido3 = Pedido.builder()
-					.cliente(cliente2)
-					.domicilioEntrega(domicilio1)
-					.estado(EstadoPedido.A_CONFIRMAR)
-					.tipoEnvio(TipoEnvio.DELIVERY)
-					.totalCosto(new BigDecimal(4000))
-					.total(new BigDecimal(8000))
-					.fechaPedido(LocalDate.of(2023, 10, 10))
-					.fechaAlta(LocalDateTime.now())
-					.horaEstimadaFinalizacion(horaEspecifica)
-					.build();
-			pedidoRepository.save(pedido3);
-			DetallePedido detalle3 = DetallePedido.builder()
-					.cantidad(4)
-					.articuloManufacturado(Cocacola)
-					.pedido(pedido1)
-					.build();
-			detallePedidoRepository.save(detalle3);
-
-
-			ArticuloInsumo articulo1 = ArticuloInsumo.builder()
-					.rubroArticulo(null)
-					.denominacion("CACA")
-					.fechaBaja(null)
-					.fechaAlta(LocalDateTime.now())
-					.precioCompra(new BigDecimal(1200))
-					.stockActual(new BigDecimal(100))
-					.stockMinimo(new BigDecimal(10))
-					.unidadMedida(null)
-					.build();
-					articuloInsumoRepository.save(articulo1);
-			ArticuloInsumo articulo2 = ArticuloInsumo.builder()
-					.rubroArticulo(null)
-					.denominacion("Lechuga")
-					.fechaBaja(null)
-					.fechaAlta(LocalDateTime.now())
-					.precioCompra(new BigDecimal(1200))
-					.stockActual(new BigDecimal(2))
-					.stockMinimo(new BigDecimal(20))
-					.unidadMedida(null)
-					.build();
-			articuloInsumoRepository.save(articulo2);
-			Pedido pedido5 = Pedido.builder()
-					.cliente(cliente2)
-					.domicilioEntrega(domicilio1)
-					.estado(EstadoPedido.PAGADO)
-					.tipoEnvio(TipoEnvio.DELIVERY)
-					.fechaAlta(LocalDateTime.now())
-					.totalCosto(new BigDecimal(1000))
-					.total(new BigDecimal(2000))
-					.fechaPedido(LocalDate.of(2023, 10, 10))
-					.horaEstimadaFinalizacion(horaEspecifica)
-					.build();
-			pedidoRepository.save(pedido5);
-			Factura factura2 = Factura.builder()
-					.fechaAlta(LocalDateTime.of(2023,02,12,12,25))
-					.fechaFacturacion(LocalDateTime.now())
-					.formaPago(FormaPago.EFECTIVO)
-					.pedido(pedido5)
-					.build();
-			facturaRepository.save(factura2);
-			DetalleFactura detallefac2 = DetalleFactura.builder()
-					.cantidad(10)
-					.articuloManufacturado(Hamburguesa)
-					.factura(factura2)
-					.build();
-			detalleFacturaRepository.save(detallefac2);
 
 		};
 	}
